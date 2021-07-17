a952bb21a9671a4b81586e04bad56e8cb1599067
VanessaLin9 <vanessa7591@gmail.com>
2021-07-17 14:15:37 +0800

    feat: first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+app.listen(port, () => {
+  console.log(`express is listen on localhast${port}!`)
+})
